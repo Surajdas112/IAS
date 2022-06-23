070931f688b0752442f1e9fe27723fb2fd86dd95
Surajdas112 <31038221+Surajdas112@users.noreply.github.com>
2022-06-23 13:49:34 +0530

    Incrementing Questions at one page

--- OnlineTestAnswersEvaluation.php
+++ OnlineTestAnswersEvaluation.php
@@ -1,15 +1,34 @@
 <?php
+include('inc/connection.php');
+session_start() ;
 
+$date = date('Y-m-d');
 
+    
+        
   
         if (isset($_POST)) {
-          print_r($_POST);
+
+
+          foreach($_POST as $qNum => $val) {
+            $userAnswer = 
+                        "INSERT INTO `online_test-answers` (`Subject`, `Time`, `Name`, `Deep_Enrollment_No`, `Email`, `Test_Number`, `Question_Number`, `User_Answer`, `Correct_Answer`) 
+                         VALUES ('INDIA POLITY','12:30:55.12345-05:00','SURAJ', 'T1000', 'a@a.a', '1', '1','".$val."','a'  )";
+
+
+                          if ($conn->query($userAnswer) === TRUE ) {
+                            $_SESSION["currentQuestion"]+=1 ;
+                            print $_SESSION["currentQuestion"];
+                            header('Location:Quiz.php');
+                          } else {
+                            print "Error: " . $userAnswer . "<br>" . $conn->error;  
+                          }
+          }
           
         } else {
           // Assume btnSubmit 
         }
       
-
 
 
 
--- OnlineTestQuestions.php
+++ OnlineTestQuestions.php
@@ -1,62 +1,65 @@
 <?php
 include('inc/header.php');
+@session_start();
 
-$i = 2;
-$quesList = "SELECT * FROM onlinetest WHERE id=" . $i;
+if (isset($_SESSION["currentQuestion"]) && !empty($_SESSION["currentQuestion"])) {
+
+        $quesList = "SELECT * FROM onlinetest WHERE id=" . $_SESSION["currentQuestion"];
 
 
-$res = mysqli_query($conn, $quesList);
-if (mysqli_num_rows($res) > 0) {
-        while ($rs = mysqli_fetch_assoc($res)) {
+        $res = mysqli_query($conn, $quesList);
+        if (mysqli_num_rows($res) > 0) {
+                while ($rs = mysqli_fetch_assoc($res)) {
 ?>
 
 
-                <html>
+                        <html>
 
-                <head>
-                       <style>
-                                table,
-                                th {
-                                        margin: auto;
-                                        border: 1px solid black;
+                        <head>
+                                <style>
+                                        table,
+                                        th {
+                                                margin: auto;
+                                                border: 1px solid black;
 
-                                }
+                                        }
 
-                                td {
-                                        padding-left: 20px;
+                                        td {
+                                                padding-left: 20px;
 
-                                }
-                        </style>
-                </head>
+                                        }
+                                </style>
+                        </head>
 
-                <body>
+                        <body>
 
-                
-                        <form action="OnlineTestAnswersEvaluation.php" method="post" id="quiz">
 
-                                <table style="width:80%">
-                                        <tr>
-                                                <td><?php echo $rs['question'];?></td>
+                                <form action="OnlineTestAnswersEvaluation.php" method="post" id="quiz">
 
-                                        </tr>
-                                        <td>
-                                               
-                                                        <input type="radio" value="<?php echo $rs['ans1'];?>" name="<?php echo $rs['id'];?>"  />&nbsp  <?php echo $rs['ans1'];?>
-		                                        <input type="radio" value="<?php echo $rs['ans2'];?>" name="<?php echo $rs['id'];?>"  />&nbsp<?php echo $rs['ans2'];?>
-			                                <input type="radio" value="<?php echo $rs['ans3'];?>" name="<?php echo $rs['id'];?>"  />&nbsp<?php echo $rs['ans3'];?>
-                                                        <input type="radio" value="<?php echo $rs['ans4'];?>" name="<?php echo $rs['id'];?>"  />&nbsp<?php echo $rs['ans4'];?>
-		                                        <input type="radio" value="no_answer" checked="checked" style="display:none;" name="<?php echo $rs['id'];?>"  />
-		
-	  
-                                        </td>
-                                        <td><input type="submit" value="Submit"></td>
-                                        <td><input type="button" value="Skip"></td>
-                                </table>
-                                
-                        </form>
-                </body>
+                                        <table style="width:80%">
+                                                <tr>
+                                                        <td><?php echo $rs['question']; ?></td>
 
-                </html>
+                                                </tr>
+                                                <td>
+
+                                                        <input type="radio" value="<?php echo $rs['ans1']; ?>" name="<?php echo $rs['id']; ?>" />&nbsp <?php echo $rs['ans1']; ?>
+                                                        <input type="radio" value="<?php echo $rs['ans2']; ?>" name="<?php echo $rs['id']; ?>" />&nbsp<?php echo $rs['ans2']; ?>
+                                                        <input type="radio" value="<?php echo $rs['ans3']; ?>" name="<?php echo $rs['id']; ?>" />&nbsp<?php echo $rs['ans3']; ?>
+                                                        <input type="radio" value="<?php echo $rs['ans4']; ?>" name="<?php echo $rs['id']; ?>" />&nbsp<?php echo $rs['ans4']; ?>
+
+
+
+                                                </td>
+                                                <td><input type="submit" value="Submit"></td>
+                                                <td><input type="button" value="Skip"></td>
+                                                <td><input type="button" value="Restart"></td>
+                                        </table>
+
+                                </form>
+                        </body>
+
+                        </html>
 
 
 
@@ -64,10 +67,11 @@ if (mysqli_num_rows($res) > 0) {
 
 
 <?php
-
-
-
+                }
         }
+}
+else{
+        $_SESSION["currentQuestion"] = 0;
 }
 
 
